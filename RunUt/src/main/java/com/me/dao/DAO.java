220e7a5abac3b775c59b523e25850ace01b5f131
mzc <miao.ze@husky.neu.edu>
2020-03-08 15:57:43 -0400

    test resource use local db

--- RunUt/src/main/java/com/me/dao/DAO.java
+++ RunUt/src/main/java/com/me/dao/DAO.java
@@ -17,11 +17,7 @@ public class DAO {
 
     private static final ThreadLocal sessionThread = new ThreadLocal();
 
-    private static final SessionFactory sessionfactory = new Configuration()
-            .setProperty("hibernate.connection.url", "jdbc:mysql://" + System.getenv("DB_HOSTNAME") + ":3306/csye6225")
-            .setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"))
-            .setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"))
-            .configure("hibernate.cfg.xml").buildSessionFactory();
+    private static final SessionFactory sessionfactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 
     protected DAO() {
     }
