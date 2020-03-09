dc5e77852d5b8249d54aa75783e2fca3e2f3715c
mzc <miao.ze@husky.neu.edu>
2020-03-09 15:35:11 -0400

    create db

--- RunUt/src/main/java/com/me/dao/DAO.java
+++ RunUt/src/main/java/com/me/dao/DAO.java
@@ -18,7 +18,7 @@ public class DAO {
     private static final ThreadLocal sessionThread = new ThreadLocal();
 
     private static final SessionFactory sessionfactory = new Configuration().configure("hibernate.cfg.xml")
-            .setProperty("hibernate.connection.url", "jdbc:mysql://" + System.getenv("DB_HOSTNAME") + ":3306/csye6225")
+            .setProperty("hibernate.connection.url", "jdbc:mysql://" + System.getenv("DB_HOSTNAME") + ":3306/csye6225?createDatabaseIfNotExist=true")
             .setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"))
             .setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"))
             .buildSessionFactory();
